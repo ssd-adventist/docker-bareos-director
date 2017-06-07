7317035b1ad87d8aa68738ac94305e64b93a977e
Ryann Micua (office) <rmicua@ssd.org>
2017-06-07 16:09:26 +0800

    add experimental repo

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --no-cache --force-rm -t ssdit/bareos-director-centos:16.2.5 -t ssdit/bareos-director-centos:latest -f ./Dockerfile-centos .
+docker build --no-cache --force-rm -t ssdit/bareos-director:experimental-17.1.3 -t ssdit/bareos-director:experimental-latest -f ./Dockerfile-experimental .
--- push-image.sh
+++ push-image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker push ssdit/bareos-director-centos:16.2.5 && docker push ssdit/bareos-director-centos:latest
+docker push ssdit/bareos-director:experimental-17.1.3 && docker push ssdit/bareos-director:experimental-latest
