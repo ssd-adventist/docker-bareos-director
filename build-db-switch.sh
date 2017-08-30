635e04b79cf26f69297cc06ae8d6cacf550dbbcb
root <root@conductor.ssd.org>
2017-08-30 07:02:18 +0000

    cleanup

--- build-db-switch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build --no-cache --force-rm -t ssdit/bareos-director:db-switch -t ssdit/bareos-director:db-switch-latest -f ./Dockerfile-db-switch .
--- push-image-db-switch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker push ssdit/bareos-director:db-switch && docker push ssdit/bareos-director:db-switch-latest
